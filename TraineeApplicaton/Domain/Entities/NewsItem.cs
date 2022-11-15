53eaa4552eb21fb6dd42171c846a9e3bff20b930
Danil Svetaylo <danil.svetaylo@gmail.com>
2022-11-15 16:58:00 +0200

    Update database

--- TraineeApplicaton/Domain/Entities/NewsItem.cs
+++ TraineeApplicaton/Domain/Entities/NewsItem.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TraineeApplication.Domain.Entities
 {
-    public class NewsItem
+    public class NewsItem : EntityBase
     {
+        [Required(ErrorMessage = "Заповніть заголовок новини")]
+        [Display(Name = "Заголовок")]
+        public override string Title { get; set; } = "Заголовок";
+        [Display(Name = "Дата новини")]
+        public override string Subtitle { get; set; } = "1 січня";
+        [Display(Name = "Текст новини")]
+        public override string Text { get; set; } = "Вміст заповнюється адміністратором";
     }
 }
---
+++ TraineeApplicaton/Migrations/20221115144716_Second.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TraineeApplication.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NewsItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    TitleImagePath = table.Column<string>(nullable: true),
+                    MetaTitle = table.Column<string>(nullable: true),
+                    MetaDescription = table.Column<string>(nullable: true),
+                    MetaKeywords = table.Column<string>(nullable: true),
+                    DateAdded = table.Column<DateTime>(nullable: false),
+                    Title = table.Column<string>(nullable: false),
+                    Subtitle = table.Column<string>(nullable: true),
+                    Text = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewsItems", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "baa165f2-90a8-452b-afda-bd8f3dd382ae",
+                column: "ConcurrencyStamp",
+                value: "be08ff0b-751c-4f94-ab44-7f1a2eb238e5");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "410fd6fc-c0eb-4671-b6b9-f245d78a8516",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "9b6f40da-7f45-477c-aafc-38eb0c8d7a34", "AQAAAAEAACcQAAAAEFK8aLFcknmcjn8Kq1vtlH9V5+SmPvpTFFKy19mj4lN0HVrvo8O/vAingoV1IV49sA==" });
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("673c93b2-866d-4885-bca2-3f66f184dff5"),
+                column: "DateAdded",
+                value: new DateTime(2022, 11, 15, 14, 47, 15, 355, DateTimeKind.Utc).AddTicks(6185));
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("a76456fb-9a51-40c8-812b-00921c81f38c"),
+                column: "DateAdded",
+                value: new DateTime(2022, 11, 15, 14, 47, 15, 355, DateTimeKind.Utc).AddTicks(6268));
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("b6de0903-3ba8-44b2-a5d9-f03d315098be"),
+                column: "DateAdded",
+                value: new DateTime(2022, 11, 15, 14, 47, 15, 355, DateTimeKind.Utc).AddTicks(3976));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NewsItems");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "baa165f2-90a8-452b-afda-bd8f3dd382ae",
+                column: "ConcurrencyStamp",
+                value: "2bf440f9-5796-4bec-889d-b91628971ae9");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "410fd6fc-c0eb-4671-b6b9-f245d78a8516",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "c4a22a09-b5ca-4c29-9b13-f3354c127a66", "AQAAAAEAACcQAAAAEP/ZPfJh5SEQ3qs0MEvC85bRNgCVHkdZCGWToGNm9s7mj4cGQgQT4SLCtNxqGF7CeA==" });
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("673c93b2-866d-4885-bca2-3f66f184dff5"),
+                column: "DateAdded",
+                value: new DateTime(2022, 11, 10, 12, 45, 45, 443, DateTimeKind.Utc).AddTicks(1995));
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("a76456fb-9a51-40c8-812b-00921c81f38c"),
+                column: "DateAdded",
+                value: new DateTime(2022, 11, 10, 12, 45, 45, 443, DateTimeKind.Utc).AddTicks(2105));
+
+            migrationBuilder.UpdateData(
+                table: "TextFields",
+                keyColumn: "Id",
+                keyValue: new Guid("b6de0903-3ba8-44b2-a5d9-f03d315098be"),
+                column: "DateAdded",
+                value: new DateTime(2022, 11, 10, 12, 45, 45, 442, DateTimeKind.Utc).AddTicks(9067));
+        }
+    }
+}
