902f1ed85eec81907c6923ed1622f5eddcb18547
Danil Svetaylo <danil.svetaylo@gmail.com>
2022-11-17 12:25:58 +0200

    Add chat controller

--- TraineeApplicaton/Controllers/ChatController.cs
+++ TraineeApplicaton/Controllers/ChatController.cs
@@ -1,16 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TraineeApplication.Domain;
+using TraineeApplication.Domain.Entities;
 
 namespace TraineeApplication.Controllers
 {
     public class ChatController : Controller
     {
-        public IActionResult Index()
+        private readonly DataManager dataManager;
+        private readonly IWebHostEnvironment hostEnvironment;
+        public ChatController(DataManager dataManager, IWebHostEnvironment hostEnvironment)
         {
-            return View();
+            this.dataManager = dataManager;
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(MessageItem model)
+        {
+            if (ModelState.IsValid)
+            {
+                dataManager.MessageItems.SaveMessageItem(model);
+                return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).Replace("Controller", string.Empty));
+            }
+            return View("Error", "Model is not valid");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Guid id)
+        {
+            dataManager.MessageItems.DeleteMessageItem(id);
+            return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).Replace("Controller", string.Empty));
         }
     }
 }
