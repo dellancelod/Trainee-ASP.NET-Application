f8b040a746cb2cad51d356e86ba9b1618a90f010
Danil Svetaylo <danil.svetaylo@gmail.com>
2022-11-18 15:39:01 +0200

    Refactor news edit view

--- TraineeApplicaton/Domain/Entities/NewsItem.cs
+++ TraineeApplicaton/Domain/Entities/NewsItem.cs
@@ -13,8 +13,6 @@ namespace TraineeApplication.Domain.Entities
         [Remote(action: "IsTitleAvailable", controller:"NewsItems", ErrorMessage = "Заголовок вже існує")]
         [Display(Name = "Заголовок")]
         public override string Title { get; set; } = "Заголовок";
-        [Display(Name = "Дата новини")]
-        public override string Subtitle { get; set; } = "1 січня";
         [Display(Name = "Текст новини")]
         public override string Text { get; set; } = "Вміст заповнюється адміністратором";
         public string AuthorID { get; set; } = "410fd6fc-c0eb-4671-b6b9-f245d78a8516";
